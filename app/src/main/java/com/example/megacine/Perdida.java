302abc3828b419a0a8e4fd0c5b367b3723a17046
Javierab202 <javabena@espol.edu.ec>
2019-11-29 16:37:53 -0500

    Subida del proyecto

--- app/src/main/java/com/example/megacine/Perdida.java
+++ app/src/main/java/com/example/megacine/Perdida.java
@@ -5,13 +5,18 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class Perdida extends AppCompatActivity {
 
+    Button btnSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perdida);
+
+        btnSalir= (Button) findViewById(R.id.button25);
+
     }
 
     public void Volver (View view){
@@ -22,9 +27,17 @@ public class Perdida extends AppCompatActivity {
     }
 
     public void Finalizar(View view){
-
-        Intent intent= new Intent();
-        System.exit(0);
-        finish();
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        //Intent intent= new Intent();
+        //System.exit(0);
+        //finish();
     }
 }
