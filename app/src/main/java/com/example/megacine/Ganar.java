66f846a4206ce3813188b15fab97ba23b2a527d0
Javierab202 <javabena@espol.edu.ec>
2019-11-29 16:51:31 -0500

    m

--- app/src/main/java/com/example/megacine/Ganar.java
+++ app/src/main/java/com/example/megacine/Ganar.java
@@ -2,22 +2,38 @@ package com.example.megacine;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 
 public class Ganar extends AppCompatActivity {
-
+    Button btnSalir;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ganar);
+
+        btnSalir=(Button) findViewById(R.id.button26);
     }
 
 
     public void Salir (View view){
-        System.exit(0);
-        finish();
+
+        btnSalir.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent= new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_HOME);
+                intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intent);
+            }
+        });
+        //Intent intent= new Intent();
+        //System.exit(0);
+        //finish();
+
     }
 
 
